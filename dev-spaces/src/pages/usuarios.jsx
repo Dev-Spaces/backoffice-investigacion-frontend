a07b75534c3eb101a3576b741e9c6c2b34a4f5b8
Jeronimo Orozco <eljeroorozcogomez@gmail.com>
2021-12-09 21:39:26 -0500

    agregar carpetas

--- dev-spaces/src/pages/usuarios.jsx
+++ dev-spaces/src/pages/usuarios.jsx
@@ -28,10 +28,10 @@ function Usuarios(){
     
     const data = [
         {nombre: 'Alfredo', identificacion: 4500, rol: 'adminstrador', estado: 'activo', correo: 'alfredo@prueba.com'},
-        {nombre: 'Alfredo', identificacion: 4500, rol: 'adminstrador', estado: 'activo', correo: 'alfredo@prueba.com'},
-        {nombre: 'Alfredo', identificacion: 4500, rol: 'adminstrador', estado: 'activo', correo: 'alfredo@prueba.com'},
-        {nombre: 'Alfredo', identificacion: 4500, rol: 'adminstrador', estado: 'activo', correo: 'alfredo@prueba.com'},
-        {nombre: 'Alfredo', identificacion: 4500, rol: 'adminstrador', estado: 'activo', correo: 'alfredo@prueba.com'}
+        {nombre: 'Santiago', identificacion: 4600, rol: 'adminstrador', estado: 'activo', correo: 'santiago@prueba.com'},
+        {nombre: 'Michael', identificacion: 8400, rol: 'adminstrador', estado: 'activo', correo: 'michael@prueba.com'},
+        {nombre: 'Juan pablo', identificacion: 6200, rol: 'adminstrador', estado: 'activo', correo: 'JuanP@prueba.com'},
+        {nombre: 'Donovan', identificacion: 3459, rol: 'adminstrador', estado: 'activo', correo: 'donovan@prueba.com'}
     ];
 
 
